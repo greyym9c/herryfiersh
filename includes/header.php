2f759d44dd7ee9e32a9d0f76ec7df579ad49e50a
User <user@example.com>
2026-01-29 17:09:16 +0700

    Fix asset loading: simplify .htaccess and add cache busting

--- includes/header.php
+++ includes/header.php
@@ -17,6 +17,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
